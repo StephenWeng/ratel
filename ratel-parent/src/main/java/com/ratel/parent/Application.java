4242dc2834347b22cdc1c92684282a3a4fa157e6
StephenWeng <403558048@qq.com>
2018-12-25 09:26:59 +0800

    ratel：父类maven项目

---
+++ ratel-parent/src/main/java/com/ratel/parent/Application.java
@@ -0,0 +1,30 @@
+package com.ratel.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author lzc
+ * @version V1.0,2017年5月10日 上午9:20:14
+ * @see [相关类/方法]
+ * @since V1.0
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * 
+	 * @Description:项目启动main方法
+	 *
+	 * @author wenzhang
+	 * @date:2018年2月9日 上午10:11:46
+	 * @Title:main
+	 * @param args
+	 * @since JDK 1.8
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
