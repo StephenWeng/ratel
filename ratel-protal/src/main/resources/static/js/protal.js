836b486d5ff1fc9cdd2fa740df03004399ed31eb
StephenWeng <403558048@qq.com>
2018-12-28 09:32:26 +0800

    调整项目代码结构

---
+++ ratel-protal/src/main/resources/static/js/protal.js
@@ -0,0 +1,19 @@
+/* 页面加载时触发事件 */
+window.onload=function(){ 
+	vm.init();
+	//页面初始化时，设置高度
+	setAsideHeight();
+}
+
+window.onresize = function(){
+	//页面改变窗口时，设置高度
+	setAsideHeight();
+}
+
+/**
+* 修改页面主体高度
+*/
+function setAsideHeight(){
+	document.getElementById("aside").style.height= (getWinHeight()-(40)) +"px";
+	document.getElementById("main").style.height= (getWinHeight()-(40+30)) +"px";
+}
